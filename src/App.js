dd80c2445d87ab935cc541ffea2d27330ebd89ab
Lucas Andrade de Oliveira <lucasandradedeveloper@gmail.com>
2019-07-11 20:59:22 +0100

    structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="p-6">
+            <header>
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
+                <a
+                    className="bg-blue-700 text-white px-6 py-2 rounded-full inline-block"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
